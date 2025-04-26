4fbdee0fe5a2cc037cc9d4c6b703bf512dce03f6
swan.benoit <swan.benoit@visiativ.com>
2025-04-26 13:18:15 +0200

    add hello function and corresponding test; include pytest in requirements

---
+++ exercises/chapter 1/1-hello/hello.py
@@ -0,0 +1,5 @@
+def hello():
+    """
+    This function prints "Bonjour le monde!" to the console.
+    """
+    print("Bonjour le monde!")
---
+++ exercises/chapter 1/1-hello/test_hello.py
@@ -0,0 +1,7 @@
+import pytest
+from hello import hello
+
+def test_hello(capsys):
+    hello()
+    captured = capsys.readouterr()
+    assert captured.out == "Bonjour le monde!\n"
