57e64c8fdaa3d6fb9a3c2c773f832c81b6aaafcf
Clément <clement.barrau@ecole-isitech.fr>
2025-07-28 12:18:38 +0200

    return Buzz ok

--- exercises/4-fizzbuzz/test_fizzbuzz.py
+++ exercises/4-fizzbuzz/test_fizzbuzz.py
@@ -4,3 +4,5 @@ def test_fizzbuzz():
      assert fizzbuzz(1) == '1'
      assert fizzbuzz(2) == '2'
      assert fizzbuzz(3) == 'Fizz'
+     assert fizzbuzz(4) == '4'
+     assert fizzbuzz(5) == 'Buzz'
