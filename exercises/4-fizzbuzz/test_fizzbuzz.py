32ab74b585a844baca3b8a674b10224a1494ded2
Clément <clement.barrau@ecole-isitech.fr>
2025-07-28 12:17:59 +0200

    return Fizz ok

--- exercises/4-fizzbuzz/test_fizzbuzz.py
+++ exercises/4-fizzbuzz/test_fizzbuzz.py
@@ -1,4 +1,6 @@
 from src import fizzbuzz
 
-# def test_fizzbuzz():
-#     assert fizzbuzz(1)
+def test_fizzbuzz():
+     assert fizzbuzz(1) == '1'
+     assert fizzbuzz(2) == '2'
+     assert fizzbuzz(3) == 'Fizz'
