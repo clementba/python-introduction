e3dbff6cae8e744749b19a605bb6ab353c365050
Clément <clement.barrau@ecole-isitech.fr>
2025-07-28 14:24:07 +0200

    test fizzbuzz ok + fonction pour chaque test

--- exercises/4-fizzbuzz/test_fizzbuzz.py
+++ exercises/4-fizzbuzz/test_fizzbuzz.py
@@ -4,5 +4,10 @@ def test_fizzbuzz():
      assert fizzbuzz(1) == '1'
      assert fizzbuzz(2) == '2'
      assert fizzbuzz(3) == 'Fizz'
+
+def test_buzz():
      assert fizzbuzz(4) == '4'
      assert fizzbuzz(5) == 'Buzz'
+
+def test_fizz_buz():
+     assert fizzbuzz(15) == 'FizzBuzz'
